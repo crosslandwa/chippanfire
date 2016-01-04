5376ab88e584aa49373f6274dbdc3e23c2125c2f
crosslandwa <crossland.w.a@gmail.com>
2016-01-04 10:59:44 +0000

    Delete *.html files from previous build;

--- src/Site.php
+++ src/Site.php
@@ -2,17 +2,27 @@
 
 class Site {
     private $_pages;
-    private $_navigation;
 
     function __construct($pages) {
         $this->_pages = $pages;
-        $this->_navigation = new Navigation($pages);
+    }
+
+    /**
+     * Remove HTML pages from previous build
+     */
+    function clearLastBuild() {
+        $oldFiles = glob('site/*.html');
+        foreach ($oldFiles as $oldFile) {
+            if (is_file($oldFile)) {
+                unlink($oldFile);
+            }
+        }
     }
 
     function render() {
         ob_start();
         foreach ($this->_pages as $page) {
-            $page->render($this->_navigation);
+            $page->render(new Navigation($this->_pages));
             file_put_contents('site/' . $page->filename(), ob_get_contents());
             ob_clean();
         }
