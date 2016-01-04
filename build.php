0b25568aa65332683923287afe527492fd6a704d
crosslandwa <crossland.w.a@gmail.com>
2016-01-04 14:28:48 +0000

    Add WhereAmI page linked from software page.

--- build.php
+++ build.php
@@ -14,12 +14,13 @@ include 'src/Page.php';
 include 'src/Site.php';
 
 $software = new Page('Software', 'software.html', 'content-software.phtml');
-$dsm = new Page('Device Snapshot Manager', 'software-m4l-device-snapshot-manager.html', 'content-devicesnapshotmanager.phtml');
+$m4lDSM = new Page('Device Snapshot Manager', 'software-m4l-device-snapshot-manager.html', 'content-devicesnapshotmanager.phtml');
+$m4lWAI = new Page('Where Am I', 'software-m4l-where-am-i.html', 'content-whereami.phtml');
 $home = new Page('ChipPanFire', 'index.html', 'content-homepage.phtml');
 $music = new Page('Music', 'music.html', 'content-music.phtml');
 $contact = new Page('Contact', 'contact.html', 'content-contact.phtml');
 
-$allPages = array($home, $music, $software, $dsm, $contact);
+$allPages = array($home, $music, $software, $m4lDSM, $m4lWAI, $contact);
 $navPages = array($home, $music, $software, $contact);
 
 $site = new Site($allPages, new Navigation($navPages));
