442f87768087d90f168759f1701b87e625c0cd69
crosslandwa <crossland.w.a@gmail.com>
2016-01-06 13:27:36 +0000

    Utilise Link::external instead of inline <a> tags for software summaries

--- src/SoftwareContentFactory.php
+++ src/SoftwareContentFactory.php
@@ -1,12 +1,20 @@
 <?php
 
 final class SoftwareContentFactory {
+    private $_maxForLive;
+    private $_abletonLive;
+    private $_maxMSP;
+
+    public function __construct() {
+        $this->_maxForLive = Link::external('Max For Live', 'http://www.ableton.com/maxforlive');
+        $this->_abletonLive = Link::external('Ableton Live', 'http://www.ableton.com');
+        $this->_maxMSP = Link::external('MaxMSP', 'http://www.cycling74.com/');
+    }
 
     public function deviceSnapshotManager() {
         return new SoftwareContent(
             'content-devicesnapshotmanager.phtml',
-            '<a href="http://www.ableton.com/maxforlive" target="_blank">Max For Live</a> device that adds '
-                . 'the ability to store and recall ‘snapshots’ of Ableton Live devices in realtime',
+            $this->_maxForLive . ' device that adds the ability to store and recall ‘snapshots’ of Ableton Live devices in realtime',
             'http://www.chippanfire.com/cpf_media/software/dsm-screenshot.jpg'
         );
     }
@@ -14,8 +22,7 @@ final class SoftwareContentFactory {
     public function whereAmI() {
         return new SoftwareContent(
             'content-whereami.phtml',
-             '<a href="http://www.ableton.com/maxforlive" target="_blank">Max For Live</a> utility device that displays '
-                . 'Live API information for the currently selected element of the Ableton Live interface',
+             $this->_maxForLive . ' utility device that displays Live API information for the currently selected element of the Ableton Live interface',
             'http://www.chippanfire.com/cpf_media/software/wai-screenshot.jpg'
         );
     }
@@ -23,7 +30,7 @@ final class SoftwareContentFactory {
     public function kmkControlScript() {
         return new SoftwareContent(
             null,
-            'In-depth control of <a href="http://www.ableton.com" target="_blank">Ableton Live</a> using the Korg Microkontrol (greatly improved on that offered natively).',
+            'In-depth control of ' . $this->_abletonLive . ' using the Korg Microkontrol (greatly improved on that offered natively).',
             'http://www.chippanfire.com/cpf_media/software/dsm-screenshot.jpg'
         );
     }
@@ -31,7 +38,7 @@ final class SoftwareContentFactory {
     public function wacNetworkMidi() {
         return new SoftwareContent(
             'content-wacnetworkmidi.phtml',
-            'Cross-platform (Win and OSX) tool built with <a rel="nofollow" href="http://www.cycling74.com/" target="_blank">MaxMSP</a> for transmitting MIDI from one computer to another via a network, without the need for hardware MIDI interfaces.',
+            'Cross-platform (Win and OSX) tool built with ' . $this->_maxMSP . ' for transmitting MIDI from one computer to another via a network, without the need for hardware MIDI interfaces.',
             'http://www.chippanfire.com/cpf_media/software/wac-network-midi.png'
         );
     }
