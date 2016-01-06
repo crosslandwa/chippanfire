c616e8847db22203c68f5d59a598a7510e2313c3
crosslandwa <crossland.w.a@gmail.com>
2016-01-06 17:02:50 +0000

    Move site creation out of build.php into static factory method on Site. Start reducing duplication

--- build.php
+++ build.php
@@ -19,31 +19,7 @@ include 'src/SoftwareContentFactory.php';
 include 'src/SoftwareHomeContent.php';
 include 'src/SoftwareSummary.php';
 
-$softwareFactory = new SoftwareContentFactory();
 
-
-// TODO yuk - sort this. All into factory?
-$m4lDSM = new Page('Device Snapshot Manager', 'software-m4l-device-snapshot-manager.html', $softwareFactory->deviceSnapshotManager(), Link::internal('Device Snapshot Manager', 'software-m4l-device-snapshot-manager.html'));
-$m4lWAI = new Page('Where Am I', 'software-m4l-where-am-i.html', $softwareFactory->whereAmI(), Link::internal('Where Am I', 'software-m4l-where-am-i.html'));
-$wacNetworkMidi = new Page('Wac Network MIDI', 'software-wac-network-midi.html', $softwareFactory->wacNetworkMidi(), Link::internal('Wac Network MIDI', 'software-wac-network-midi.html'));
-$kmkControlScript = new Page('KMK Control Script', 'https://github.com/crosslandwa/kmkControl', $softwareFactory->kmkControlScript(), Link::external('KMK Control Script', 'https://github.com/crosslandwa/kmkControl'));
-
-$softwareSummaries = array(
-    new SoftwareSummary($m4lDSM, $softwareFactory->deviceSnapshotManager()),
-    new SoftwareSummary($m4lWAI, $softwareFactory->whereAmI()),
-    new SoftwareSummary($kmkControlScript, $softwareFactory->kmkControlScript()),
-    new SoftwareSummary($wacNetworkMidi, $softwareFactory->wacNetworkMidi())
-);
-
-$software = new Page('Software', 'software.html', new SoftwareHomeContent($softwareSummaries), Link::internal('Software', 'software.html'));
-
-$home = new Page('ChipPanFire', 'index.html', new SimpleContent('content-homepage.phtml'), Link::internal('ChipPanFire', 'index.html'));
-$music = new Page('Music', 'music.html', new SimpleContent('content-music.phtml'), Link::internal('Music', 'music.html'));
-$contact = new Page('Contact', 'contact.html', new SimpleContent('content-contact.phtml'), Link::internal('Contact', 'contact.html'));
-
-$allPages = array($home, $music, $software, $m4lDSM, $m4lWAI, $wacNetworkMidi, $contact);
-$navPages = array($home, $music, $software, $contact);
-
-$site = new Site($allPages, new Navigation($navPages));
+$site = Site::create();
 $site->clearLastBuild();
 $site->render();
--- src/Site.php
+++ src/Site.php
@@ -4,9 +4,55 @@ final class Site {
     private $_pages;
     private $_navigation;
 
-    public function __construct($pages, $navigation) {
+    private function __construct($pages, $navigation) {
         $this->_pages = $pages;
         $this->_navigation = $navigation;
+    }
+
+    public final static function create() {
+        $softwareFactory = new SoftwareContentFactory();
+
+        $pagesMeta = array(
+            'm4lDSM' => array(
+                'title' => 'Device Snapshot Manager',
+                'href' => 'software-m4l-device-snapshot-manager.html',
+                'content' => $softwareFactory->deviceSnapshotManager()
+            ),
+            'm4lWAI' => array(
+                'title' => 'Where Am I',
+                'href' => 'software-m4l-where-am-i.html',
+                'content' => $softwareFactory->whereAmI()
+            )
+        );
+
+        $pages = array();
+        foreach ($pagesMeta as $key => $p) {
+            // TODO internal/external pages
+            $pages[$key] = new Page($p['title'], $p['href'], $p['content'], Link::internal($p['title'], $p['href']));
+        }
+
+        // TODO keep moving into above pattern
+        $wacNetworkMidi = new Page('Wac Network MIDI', 'software-wac-network-midi.html', $softwareFactory->wacNetworkMidi(), Link::internal('Wac Network MIDI', 'software-wac-network-midi.html'));
+        $kmkControlScript = new Page('KMK Control Script', 'https://github.com/crosslandwa/kmkControl', $softwareFactory->kmkControlScript(), Link::external('KMK Control Script', 'https://github.com/crosslandwa/kmkControl'));
+
+        // TODO duplicate creation/use of content here...
+        $softwareSummaries = array(
+            new SoftwareSummary($pages['m4lDSM'], $softwareFactory->deviceSnapshotManager()),
+            new SoftwareSummary($pages['m4lWAI'], $softwareFactory->whereAmI()),
+            new SoftwareSummary($kmkControlScript, $softwareFactory->kmkControlScript()),
+            new SoftwareSummary($wacNetworkMidi, $softwareFactory->wacNetworkMidi())
+        );
+
+        $software = new Page('Software', 'software.html', new SoftwareHomeContent($softwareSummaries), Link::internal('Software', 'software.html'));
+
+        $home = new Page('ChipPanFire', 'index.html', new SimpleContent('content-homepage.phtml'), Link::internal('ChipPanFire', 'index.html'));
+        $music = new Page('Music', 'music.html', new SimpleContent('content-music.phtml'), Link::internal('Music', 'music.html'));
+        $contact = new Page('Contact', 'contact.html', new SimpleContent('content-contact.phtml'), Link::internal('Contact', 'contact.html'));
+
+        $allPages = array($home, $music, $software, $pages['m4lDSM'], $pages['m4lWAI'], $wacNetworkMidi, $contact);
+        $navPages = array($home, $music, $software, $contact);
+
+        return new Site($allPages, new Navigation($navPages));
     }
 
     /**
