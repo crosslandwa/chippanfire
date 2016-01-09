b601810fb17c2222c5bf8286aed9516f402b585b
Will <crossland.w.a@gmail.com>
2016-01-09 23:43:10 +0000

    Move documentation downloads into site/assets/downloads/documentation

--- src/Documentation.php
+++ src/Documentation.php
@@ -8,7 +8,7 @@ class Documentation {
     }
 
     public function __toString() {
-        return '<form action="http://www.chippanfire.com/cpf_media/software/' . $this->_filename . '" method="GET">'
+        return '<form action="assets/downloads/documentation/' . $this->_filename . '" method="GET">'
             . '<button type="submit" class="btn btn-success">Download Documentation</button>'
             . '</form>';
     }
