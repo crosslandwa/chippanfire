d28d790a43d6826321554831d884d90bfd32fa2e
Will <crossland.w.a@gmail.com>
2016-07-02 23:28:14 +0100

    Pulse button when ticks occur

--- metronome-app/app.js
+++ metronome-app/app.js
@@ -1,21 +1,21 @@
 const audio_context = new AudioContext(),
     Player = require('./player.js'),
-    Repeater = require('./repeater.js');
+    Repeater = require('./repeater.js'),
+    EventEmitter = require('events'),
+    util = require('util');
 
 window.addEventListener('load', off_we_go);
 
 function off_we_go() {
     let button = document.getElementById('metronome-on-off'),
-        metronome = new Metronome(
-            () => metronome_on(button),
-            () => metronome_off(button) 
-        );
-
+        metronome = new Metronome();
 
     window.addEventListener("keypress", (event) => {
         if (32 == event.charCode) metronome.toggle();
     });
     button.addEventListener("click", metronome.toggle);
+    metronome.on('tick_start', () => metronome_on(button));
+    metronome.on('tick_stop', () => metronome_off(button));
 }
 
 function metronome_on(ui_btn) {
@@ -26,12 +26,14 @@ function metronome_off(ui_btn) {
     ui_btn.classList.remove('active');
 }
 
-function Metronome(on_start, on_stop) {
+function Metronome() {
+    EventEmitter.call(this);
     let active = false,
         accent = new Player('assets/audio/metronome-accent.mp3', audio_context),
         tick = new Player('assets/audio/metronome-tick.mp3', audio_context),
         repeater = Repeater.create_scheduled_by_audio_context(audio_context, 500),
-        count = 0;
+        count = 0,
+        metronome = this;
 
     let pulse = function() {
         if (count == 0) {
@@ -46,11 +48,15 @@ function Metronome(on_start, on_stop) {
         active = !active;
         if (active) {
             repeater.start(pulse);
-            on_start();
         } else {
             repeater.stop();
             count = 0;
-            on_stop();
         }
     }
+
+    accent.on('started', () => metronome.emit('tick_start'));
+    tick.on('started', () => metronome.emit('tick_start'));
+    accent.on('stopped', () => metronome.emit('tick_stop'));
+    tick.on('stopped', () => metronome.emit('tick_stop'));
 }
+util.inherits(Metronome, EventEmitter);
