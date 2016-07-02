dde06717a1d223a7eb94c6d6f0295c3ee054eb8e
Will <crossland.w.a@gmail.com>
2016-07-02 10:41:40 +0100

    Add ability to render custom scripts in header

--- src/Header.php
+++ src/Header.php
@@ -1,7 +1,13 @@
 <?php
 
 final class Header {
+    private $_customScripts = array();
+
     public function render() {
         require 'template/header.phtml';
+    }
+
+    public function addScript($filename) {
+        $this->_customScripts[] = $filename;
     }
 }
