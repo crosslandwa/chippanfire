2a33ecd58468c5c9bbdb0c0a211488c5572a5d25
Will <crossland.w.a@gmail.com>
2016-07-02 23:59:48 +0100

    Add BPM controlled via slider

--- metronome-app/app.js
+++ metronome-app/app.js
@@ -8,11 +8,21 @@ window.addEventListener('load', off_we_go);
 
 function off_we_go() {
     let button = document.getElementById('metronome-on-off'),
-        metronome = new Metronome();
+        bpm = document.getElementById('bpm-control'),
+        bpm_label = document.querySelector("label[for='bpm-control']")
+        metronome = new Metronome(120);
+
 
     window.addEventListener("keypress", (event) => {
         if (32 == event.charCode) metronome.toggle();
     });
+
+    bpm.addEventListener("input", (event) => {
+        const new_bpm = event.target.value;
+        bpm_label.innerHTML = 'BPM: ' + new_bpm;
+        metronome.update_bpm(new_bpm)
+    })
+
     button.addEventListener("click", metronome.toggle);
     metronome.on('tick_start', () => metronome_on(button));
     metronome.on('tick_stop', () => metronome_off(button));
@@ -26,13 +36,19 @@ function metronome_off(ui_btn) {
     ui_btn.classList.remove('active');
 }
 
-function Metronome() {
+function Metronome(initial_bpm) {
     EventEmitter.call(this);
+
+    let bpm_to_ms = function(bpm) {
+        return (60 / bpm) * 1000;
+    }
+
     let active = false,
         accent = new Player('assets/audio/metronome-accent.mp3', audio_context),
         tick = new Player('assets/audio/metronome-tick.mp3', audio_context),
-        repeater = Repeater.create_scheduled_by_audio_context(audio_context, 500),
+        repeater = Repeater.create_scheduled_by_audio_context(audio_context, bpm_to_ms(initial_bpm)),
         count = 0,
+        bpm = initial_bpm
         metronome = this;
 
     let pulse = function() {
@@ -42,6 +58,10 @@ function Metronome() {
             tick.play(1, 20000);
         }
         count = (++count % 4);
+    }
+
+    this.update_bpm = function(bpm) {
+        repeater.interval(bpm_to_ms(bpm))
     }
 
     this.toggle = function() {
