afa8987e246bb11f9d2909a2a2512fd9812c68f8
Will <crossland.w.a@gmail.com>
2016-07-18 11:13:54 +0100

    Redirect http traffic to https

--- src/OronjoBuy.php
+++ src/OronjoBuy.php
@@ -11,7 +11,7 @@ final class OronjoBuy {
 
     public final function __toString() {
         return '<a class="btn btn-default" href="http://www.oronjo.com/live/next/?fi=' . $this->_itemId . '" target="_blank">'
-            . '<img src="http://www.oronjo.com/live/images/image.php?id=' . $this->_itemId . '" border="0" alt="">'
+            . '<img src="https://www.oronjo.com/live/images/image.php?id=' . $this->_itemId . '" border="0" alt="">'
             . '<br>' . $this->_text
             . '</a>';
     }
