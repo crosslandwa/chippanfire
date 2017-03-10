497fe5da5dd935b6f697ae2966733ad76efdf48d
crosslandwa <crossland.w.a@gmail.com>
2017-03-10 22:06:49 +0000

    JS re-direct if error page is served from wrong path
    Of course, this won't work as the relative link to the js that does the
    re-direct will 404...

--- assets/error-page.js
+++ assets/error-page.js
@@ -1,29 +1,48 @@
+const contains = path => test => path.indexOf(test) != -1
 
-function lookingFor(path) {
-    var suggestedName;
-    var suggestHref;
-    switch (true) {
-        case path.indexOf('carsonbrent') != -1:
-        case path.indexOf('kilclinton') != -1:
-        case path.indexOf('soccochico') != -1:
-        case path.indexOf('music') != -1:
-            suggestHref = 'music.html'
-            suggestedName = 'music'
-            break;
-        case path.indexOf('software') != -1:
-            suggestHref = 'software.html'
-            suggestedName = 'software'
-            break;
-        case path.indexOf('contact') != -1:
-            suggestHref = 'contact.html'
-            suggestedName = 'contact'
-            break;
-    }
+function redirectIfNotErrorPage(path) {
+  if (!path.endsWith('error.html')) {
+    window.location.replace(`http://localhost:8000/error.html?search=${encodeURIComponent(path)}`);
+    return true
+  }
+  return false
+}
+
+function lookingFor(href) {
+  var urlContains = contains(href.toLowerCase());
+  var suggestedName;
+  var suggestHref;
+  switch (true) {
+    case urlContains('carson'):
+    case urlContains('brent'):
+    case urlContains('kilclinton'):
+    case urlContains('socco'):
+    case urlContains('chico'):
+    case urlContains('music'):
+    case urlContains('thing'):
+      suggestHref = 'music.html'
+      suggestedName = 'music'
+      break;
+    case urlContains('midi'):
+    case urlContains('miniak'):
+    case urlContains('where'):
+    case urlContains('snapshot'):
+    case urlContains('modulo'):
+    case urlContains('software'):
+    case urlContains('kmk'):
+      suggestHref = 'software.html'
+      suggestedName = 'software'
+      break;
+    case urlContains('contact'):
+      suggestHref = 'contact.html'
+      suggestedName = 'contact'
+      break;
+  }
     if (suggestHref) {
-        document.getElementById("redirect-suggestion").innerHTML = '...or perhaps you were looking for <a href="https://chippanfire.com/' + suggestHref + '">' + suggestedName + '</a>?';
+      document.getElementById('redirect-suggestion').innerHTML = `...or were looking for <a href="https://chippanfire.com/${suggestHref}">${suggestedName}</a>?`;
     }
 }
 
 window.addEventListener('load', function () {
-    lookingFor(window.location.pathname);
+  redirectIfNotErrorPage(window.location.pathname) || lookingFor(window.location.href);
 })
