c691e8123a26df8115cbaebb4924f149c6f5eec7
will.crossland <will.crossland01@bbc.co.uk>
2020-01-11 08:53:10 +0000

    Italicise

--- src/miniak-patch-editor/index.js
+++ src/miniak-patch-editor/index.js
@@ -14,13 +14,13 @@ const MiniakPatchEditor = () => (
     <h3 className="cpf-header cpf-header--small">
       <ExternalLink href="https://github.com/crosslandwa/miniak-patch-editor">Download from Github</ExternalLink>
     </h3>
-    <p>Miniak Patch Editor is a <ExternalLink href="https://www.cycling74.com/">MaxMSP</ExternalLink> patch that can be downloaded and used <strong>for free!</strong></p>
+    <p>Miniak Patch Editor is a <ExternalLink href="https://www.cycling74.com/">MaxMSP</ExternalLink> patch that can be downloaded and used <em>for free!</em></p>
     <p>The download includes:</p>
     <ul>
       <li>The Miniak Patch Editor MaxMSP patch</li>
       <li>A template for controlling Miniak Patch Editor from the popular iDevice software
-        <ExternalLink href="https://hexler.net/software/touchosc">TouchOSC</ExternalLink>, created by <strong>Adam
-        Neddo</strong> (see documentation for contact details). <em>Note the template controls the editor
+        <ExternalLink href="https://hexler.net/software/touchosc">TouchOSC</ExternalLink>, created by <em>Adam
+        Neddo</em> (see documentation for contact details). <em>Note the template controls the editor
         software (which updates the Miniak in realtime), but not the Miniak directly</em>
       </li>
       <li>Documentation for setup and use</li>
