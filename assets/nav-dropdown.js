514649764f5d5dcc2a8438ba1781a448eafd3232
will.crossland <will.crossland01@bbc.co.uk>
2020-01-11 09:29:19 +0000

    Delete unused assets and templates from old build/site

--- assets/nav-dropdown.js
+++
@@ -1,7 +0,0 @@
-window.onclick = event => {
-  if (!event.target.matches('.dropdown-toggle')) {
-    [].forEach.call(document.getElementsByClassName('dropdown'), dropdown => {
-      dropdown.classList.remove('open')
-    })
-  }
-}
