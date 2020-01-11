97ed828d83a8e6cf33c57dd70e49ca8d09b11f28
will.crossland <will.crossland01@bbc.co.uk>
2020-01-11 08:32:10 +0000

    Refactor to pass children as named attributes

--- src/max-for-live/index.js
+++ src/max-for-live/index.js
@@ -2,17 +2,16 @@ import React from 'react'
 import PageTemplate from '../PageTemplate'
 import ExternalLink from '../external-link'
 
-const Device = ({ children, documentation, download, id, title }) => {
-  const childArray = React.Children.toArray(children)
+const Device = ({ children, documentation, download, downloadInfo, id, strapline, title, versionHistory }) => {
   return (
     <>
       <h2 id={id} className="cpf-header">{title}</h2>
-      {childArray[0]}
+      {strapline}
       <div className="cpf-grid__row">
-        <div className={childArray[2] ? 'cpf-grid__column-one-half' : ''}>
+        <div className={versionHistory ? 'cpf-grid__column-one-half' : ''}>
           <h3 className="cpf-header cpf-header--small">Download</h3>
-          {childArray[1]}
-          <p>It is known to work with Ableton Live 8, but I've not updated for a very loooong time</p>
+          {downloadInfo}
+          <p>It is known to work with Ableton Live 8, but has not been updated for a very loooong time.</p>
           <a href={download} >
             <button type="submit" className="cpf-btn">Download</button>
           </a>
@@ -20,10 +19,10 @@ const Device = ({ children, documentation, download, id, title }) => {
             <button type="submit" className="cpf-btn cpf-btn--secondary">Download Documentation</button>
           </form>
         </div>
-        {childArray[2] && (
+        {versionHistory && (
           <div className="cpf-grid__column-one-half">
             <h3 className="cpf-header cpf-header--small">Version History</h3>
-            {childArray[2]}
+            {versionHistory}
           </div>
         )}
       </div>
@@ -36,62 +35,78 @@ const MaxForLiveDevices = props => (
     <h1 className="cpf-header">Max For Live Devices</h1>
     <p><ExternalLink href="https://www.ableton.com/maxforlive">Max For Live</ExternalLink> devices I have made</p>
 
-    <Device id="device-snapshot-manager" title="Device Snapshot Manager" download="m4l-device-snapshot-manager-v1-1-1.zip" documentation="DeviceSnapshotManager.pdf">
-      <>
+    <Device
+      id="device-snapshot-manager"
+      title="Device Snapshot Manager"
+      download="m4l-device-snapshot-manager-v1-1-1.zip"
+      documentation="DeviceSnapshotManager.pdf"
+      strapline={(
         <p>
-          Adds the ability to store and recall ‘snapshots’ of Ableton Live devices in realtime
+          Adds the ability to store and recall ‘snapshots’ of Ableton Live devices in realtime.
           <img src="dsm-screenshot.jpg" alt="Device Snapshot Manager" className="cpf-image cpf-image--fit-to-width"/>
         </p>
-      </>
-      <>
+      )}
+      downloadInfo={(
         <p>Device Snapshot Manager is distributed as two fully editable Max For Live .amxd files – one MIDI effect and one Audio effect.</p>
-      </>
-      <>
+      )}
+      versionHistory={(
+        <>
         <h4>1.1.1</h4>
-        <ul>
-          <li>Fixed bug where snapshots with more than 128 parameters were not fully recalled</li>
-          <li>Improved speed of JS-based Live API interactions</li>
-        </ul>
-        <h4>1.1.0</h4>
-        <ul>
-          <li> Added ability to name snapshots</li>
-        </ul>
-      </>
-    </Device>
+          <ul>
+            <li>Fixed bug where snapshots with more than 128 parameters were not fully recalled</li>
+            <li>Improved speed of JS-based Live API interactions</li>
+          </ul>
+          <h4>1.1.0</h4>
+          <ul>
+            <li> Added ability to name snapshots</li>
+          </ul>
+        </>
+      )}
+    />
 
     <hr className="cpf-section-break"/>
 
-    <Device id="where-am-i" title="Where Am I" download="m4l-where-am-i-v1-1-0.zip" documentation="WhereAmI.pdf">
-      <>
+    <Device
+      id="where-am-i"
+      title="Where Am I"
+      download="m4l-where-am-i-v1-1-0.zip"
+      documentation="WhereAmI.pdf"
+      strapline={(
         <p>
-          Device that displays Live API information for the currently selected element of the Ableton Live interface
+          Displays Live API information for the currently selected element of the Ableton Live interface.
           <img src="wai-screenshot.jpg" alt="Where Am I" className="cpf-image cpf-image--fit-to-width"/>
         </p>
-      </>
-      <>
+      )}
+      downloadInfo={(
         <p>Where Am I is distributed as a free Max For Live Audio effect (the download also includes documentation). The downloaded .amxd file is fully editable and re-usable.</p>
-      </>
-      <>
-        <h4>1.1.0</h4>
-        <ul>
-          <li>Device no longer fills Lives undo buffer when observing changes in selected track/device/etc</li>
-        </ul>
-      </>
-    </Device>
+      )}
+      versionHistory={(
+        <>
+          <h4>1.1.0</h4>
+          <ul>
+            <li>Device no longer fills Lives undo buffer when observing changes in selected track/device/etc</li>
+          </ul>
+        </>
+      )}
+    />
 
     <hr className="cpf-section-break"/>
 
-    <Device id="midi-clip-modulo" title="MIDI Clip Modulo" download="m4l-midi-clip-modulo-v1-0-0.zip" documentation="MidiClipModulo.pdf">
-      <>
+    <Device
+      id="midi-clip-modulo"
+      title="MIDI Clip Modulo"
+      download="m4l-midi-clip-modulo-v1-0-0.zip"
+      documentation="MidiClipModulo.pdf"
+      strapline={(
         <p>
           Device that adds extra capabilities to note editing in Ableton Live's MIDI clips
           <img src="midi-clip-modulo.jpg" alt="MIDI Clip Modulo" className="cpf-image cpf-image--fit-to-width"/>
         </p>
-      </>
-      <>
-      <p>Midi Clip Modulo's functionality can be mapped to keyboard/MIDI shortcuts to quickly integrate into your clip editing workflow. The device always makes edits to the currently selected MIDI clip, and all edits are fully undo-able via Lives undo function</p>
-      </>
-    </Device>
+      )}
+      downloadInfo={(
+        <p>Midi Clip Modulo's functionality can be mapped to keyboard/MIDI shortcuts to quickly integrate into your clip editing workflow. The device always makes edits to the currently selected MIDI clip, and all edits are fully undo-able via Lives undo function</p>
+      )}
+    />
 
   </PageTemplate>
 )
