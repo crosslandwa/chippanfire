71e3ea97ddc5d9fade775a252142aed620ce8d02
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2020-01-12 14:45:31 +0000

    Move favicon <link> above <title>. Also make it not self-closing

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@ const html = css => component => `<!DOCTYPE html>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <title>chippanfire.com</title>
-    <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <style>
 ${css}
     </style>
