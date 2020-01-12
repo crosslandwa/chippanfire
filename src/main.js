556f3c4730e383e4b3964c735f62ac4ac83a1010
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2020-01-12 14:38:52 +0000

    Update favicon <link> tag

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const html = css => component => `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>chippanfire.com</title>
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <style>
 ${css}
     </style>
